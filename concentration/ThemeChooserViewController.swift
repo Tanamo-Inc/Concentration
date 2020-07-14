945b3b3f0e04731783fd93d63451261769d41074
Tandoh <anthony.tandoh@tanacom.io>
2020-07-14 14:47:42 +0000

    commit UISplitViewControllerDelegate  in  ThemeChooserViewController.

--- concentration/ThemeChooserViewController.swift
+++ concentration/ThemeChooserViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ThemeChooserViewController: UIViewController {
+class ThemeChooserViewController: UIViewController,UISplitViewControllerDelegate  {
     
     
     let themes = [
@@ -20,14 +20,47 @@ class ThemeChooserViewController: UIViewController {
     ]
     
     
-    @IBAction func changeTheme(_ sender: Any) {
-        
-          performSegue(withIdentifier: "Choose Theme", sender: sender)
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
     }
     
     
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+         if let cvc = secondaryViewController as? ConcentrationViewController {
+             if cvc.theme == nil {
+                 return true
+             }
+         }
+         return false
+     }
     
     
+    @IBAction func changeTheme(_ sender: Any) {
+        
+        if let cvc = splitViewDetailConcentrationViewController {
+                   if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                       cvc.theme = theme
+                   }
+               }
+            
+            else if let cvc = lastSeguedConcentrationViewController {
+                       if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                           cvc.theme = theme
+                       }
+                       navigationController?.pushViewController(cvc, animated: true)
+                   }
+        
+        else{
+          performSegue(withIdentifier: "Choose Theme", sender: sender)
+    }
+    
+}
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+           return splitViewController?.viewControllers.last as? ConcentrationViewController
+       }
+    
+    private var lastSeguedConcentrationViewController : ConcentrationViewController?
     
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -38,7 +71,7 @@ class ThemeChooserViewController: UIViewController {
                 
                         if let cvc = segue.destination as? ConcentrationViewController {
                             cvc.theme = theme
-                          //  lastSeguedConcentrationViewController = cvc
+                            lastSeguedConcentrationViewController = cvc
                         }
                     }
               
